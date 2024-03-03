548676a954a32c712980e64d1913af438b32ceb9
Jack Bogart <jackbogart0822@gmail.com>
2024-03-03 04:18:43 -0500

    Add calculator functionality
    Users can now input numbers and operators and get their results
    displayed. Will need to work out specific edge cases and testing.

--- js/scripts.js
+++ js/scripts.js
@@ -14,13 +14,18 @@ function divide(a, b) {
     return a / b;
 }
 
+let firstNum = 0;
+let secondNum = 0;
+let currentOperation = '';
+const display = document.querySelector('#display');
+
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
         case '/':
             return divide(a, b);
@@ -29,5 +34,44 @@ function operate(operator, a, b) {
     }
 }
 
+function updateDisplayDigit(digit) {
+    // If the operation isn't null, we're updating the second number
+    if (!currentOperation) {
+        firstNum += digit;
+    } else {
+        secondNum += digit;
+    }
+    display.textContent += digit;
+}
+
+function updateDisplayOperator(operator) {
+    // If the operation isn't null, we're calculating the current output
+    if (currentOperation) {
+        firstNum = operate(currentOperation, parseInt(firstNum), parseInt(secondNum));
+        display.textContent = firstNum;
+        secondNum = 0;
+    }
+    currentOperation = operator;
+    display.textContent += currentOperation;
+}
+
 const digits = document.querySelectorAll('#digits > button');
-digits.forEach((button) => button.addEventListener('click', (e) => console.log(e.target.textContent)));
+digits.forEach((button) => button.addEventListener('click', (e) => updateDisplayDigit(e.target.textContent)));
+
+const operations = document.querySelectorAll('#operations');
+operations.forEach((button) => {
+    button.addEventListener('click', (e) => updateDisplayOperator(e.target.textContent));
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', function () {
+    updateDisplayOperator('');
+});
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    firstNum = 0;
+    secondNum = 0;
+    currentOperation = '';
+    display.textContent = '';
+});
