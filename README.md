26bc0a1cba55331d52943da21045cb4df3bdecb3
Jack Bogart <jackbogart0822@gmail.com>
2024-03-06 03:59:56 -0500

    Add rationals, backspaces, and keyboard support
    Users previously had no way to input a rational number, only integers.
    Users can now enter their own decimals. Users may also backspace the
    last digit added to a number. Lastly keyboard support is partially
    implemented. Need to fully flesh it out for all buttons before it's
    considered complete.

---
+++ README.md
@@ -0,0 +1,5 @@
+## To-Do
+
+-   Clean up how operators are displayed
+
+-   If a new operator is chosen and a second number isn't inputted, change operation
--- js/scripts.js
+++ js/scripts.js
@@ -41,26 +41,53 @@ function operate(operator, a, b) {
 function updateDisplayDigit(digit) {
     // If the operation isn't null, we're updating the second number
     if (!currentOperation) {
-        firstNum += digit;
+        firstNum = Number(firstNum + digit).toString();
+        display.textContent = firstNum;
     } else {
-        secondNum += digit;
+        secondNum = Number(secondNum + digit).toString();
+        display.textContent = secondNum;
     }
-    display.textContent += digit;
 }
 
 function updateDisplayOperator(operator) {
     // If the operation isn't null, we're calculating the current output
-    if (currentOperation) {
+    if (currentOperation && secondNum != 0) {
+        // TO-DO: Need to handle empty second input differently
         firstNum = operate(currentOperation, Number(firstNum), Number(secondNum));
-        if (!Number.isInteger(firstNum)) firstNum = firstNum.toFixed(5);
+        if (!Number.isInteger(firstNum)) {
+            minimumDecimalPlaces = Math.min(
+                firstNum.toString().substring(firstNum.toString().indexOf('.') + 1).length,
+                6
+            );
+            firstNum = firstNum.toFixed(minimumDecimalPlaces);
+        }
         display.textContent = firstNum;
         secondNum = 0;
     }
     currentOperation = operator;
-    display.textContent += currentOperation;
 }
 
-const digits = document.querySelectorAll('#digits > button');
+function backspace() {
+    if (!currentOperation) {
+        firstNum = Number(firstNum.toString().slice(0, -1));
+        display.textContent = firstNum;
+    } else {
+        secondNum = Number(secondNum.toString().slice(0, -1));
+        display.textContent = secondNum;
+    }
+}
+
+function addDecimal() {
+    if (!currentOperation && !firstNum.toString().includes('.')) {
+        firstNum += '.';
+        display.textContent = firstNum;
+    } else if (currentOperation && !secondNum.toString().includes('.')) {
+        secondNum += '.';
+        display.textContent = secondNum;
+    }
+}
+
+const digits = document.querySelectorAll('#digits > button:not(.special)');
 digits.forEach((button) => button.addEventListener('click', (e) => updateDisplayDigit(e.target.textContent)));
 
 const operations = document.querySelectorAll('#operations');
@@ -78,5 +105,26 @@ clear.addEventListener('click', () => {
     firstNum = 0;
     secondNum = 0;
     currentOperation = '';
-    display.textContent = '';
+    display.textContent = '0';
+});
+
+const del = document.querySelector('#delete');
+del.addEventListener('click', () => {
+    backspace();
+});
+
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', () => {
+    addDecimal();
+});
+
+addEventListener('keydown', (event) => {
+    if (!isNaN(event.key)) {
+        updateDisplayDigit(event.key);
+    } else if (event.key === 'Backspace') {
+        backspace();
+    } else if (event.key === 'Enter') {
+        updateDisplayOperator('');
+    }
+    console.log(event.key);
 });
